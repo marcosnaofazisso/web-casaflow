adee6212aa73615054f01f90624f21534439a7ca
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2024-01-27 12:05:27 -0300

    WIP for delete user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,100 @@
-import './App.css'
-import { NavLink } from 'react-router-dom'
+import qrCode from "./assets/img/qrcode.jpeg";
+import imageBanner from "./assets/img/new-casaflow-banner.png";
+import playStore from "./assets/img/play_store.png";
 
-function App() {
+export default function App() {
+  const goToPlayStore = () => {
+    window.location.href =
+      "https://play.google.com/store/apps/details?id=com.marcosviniciusferreira.casaflow;";
+  };
 
   return (
-    <>
-      <h1>Hello world!</h1>
-      <NavLink to="/deletar-conta">Deletar Conta</NavLink>
-    </>
-  )
+    <div
+      style={{
+        maxWidth: "100%",
+        height: "100%",
+        textAlign: "center",
+        padding: "4rem 5rem 0 5rem",
+        background: "linear-gradient(to right, #3861C9 5%, #56CAFB 95%)",
+      }}
+    >
+      <div style={{ maxWidth: "50vw", margin: "0 auto" }}>
+        <h1 style={{ fontSize: "5rem", color: "#FFF", marginBottom: "2rem" }}>
+          CasaFlow
+        </h1>
+        <span
+          style={{
+            display: "block",
+            fontSize: "2rem",
+            color: "#FFF",
+            marginBottom: "2rem",
+          }}
+        >
+          Se você busca uma solução intuitiva, gratuita e fácil de usar para
+          organizar suas finanças, encontrou o aplicativo perfeito - o CasaFlow!
+        </span>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row-reverse",
+          borderRadius: "10px",
+          justifyContent: "center",
+          background: "linear-gradient(to right, #FFF 10%, #EAEDF2)",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginTop: "8rem",
+            alignItems: "center",
+            marginRight: "20rem",
+          }}
+        >
+          <span
+            style={{
+              marginBottom: "1rem",
+              color: "#FFF",
+              background: "linear-gradient(to right, #3861C9 5%, #56CAFB 95%)",
+              padding: "1rem",
+              borderRadius: "5px",
+              fontSize: "2rem",
+            }}
+          >
+            Baixe o app gratis!
+          </span>
+          <img
+            src={qrCode}
+            width={300}
+            height={300}
+            alt="QR Code do aplicativo CasaFlow"
+            style={{ borderRadius: "10px" }}
+          />
+          <img
+            src={playStore}
+            width={280}
+            height={"auto"}
+            alt="QR Code do aplicativo CasaFlow"
+            onClick={() => goToPlayStore()}
+            style={{
+              cursor: "pointer",
+              marginTop: "1rem",
+              borderRadius: "2px",
+            }}
+          />
+        </div>
+        <div>
+          <img
+            src={imageBanner}
+            style={{ marginRight: "10rem" }}
+            width={600}
+            height={800}
+            alt="Banner com a imagem do app CasaFlow"
+          />
+        </div>
+      </div>
+      {/* <NavLink to="/deletar-conta">Deletar Conta</NavLink> */}
+    </div>
+  );
 }
-
-export default App
--- src/DeleteAccount.jsx
+++ src/DeleteAccount.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
+import { useRef, useState } from "react";
 
 export default function DeleteAccount() {
-    return (
-        <>
-            <div>Delete Account</div>
-            <NavLink to="/">go back</NavLink>
-        </>
-    )
+  const [dataDeleted, setDataDeleted] = useState(false);
+  const emailRef = useRef(null);
+
+  const deleteUserData = (event) => {
+    event.preventDefault();
+    const userData = emailRef.current.value;
+    console.log("deleteUserData ===>>", userData);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          maxWidth: "100%",
+          height: "950dvh",
+          textAlign: "center",
+          padding: "4rem 5rem 0 5rem",
+          background: "linear-gradient(to right, #3861C9 5%, #56CAFB 95%)",
+        }}
+      >
+        <div style={{ maxWidth: "50vw", margin: "0 auto" }}>
+          <h1 style={{ fontSize: "5rem", color: "#FFF", marginBottom: "2rem" }}>
+            CasaFlow
+          </h1>
+          <span
+            style={{
+              display: "block",
+              fontSize: "2rem",
+              color: "#FFF",
+              marginBottom: "2rem",
+            }}
+          >
+            Deseja apagar seus dados? Sem problemas!
+          </span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            borderRadius: "10px",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "linear-gradient(to right, #FFF 10%, #EAEDF2)",
+          }}
+        >
+          {!dataDeleted && (
+            <form
+              onSubmit={deleteUserData}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "2rem",
+                width: "40%",
+              }}
+            >
+              <input
+                type="email"
+                name="email"
+                id="email"
+                ref={emailRef}
+                required
+                pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+                title="Por favor, digite um e-mail valido"
+                style={{
+                  padding: "1rem 3rem",
+                  borderRadius: "8px",
+                  marginBottom: "2rem",
+                }}
+              />
+              <button
+                type="submit"
+                style={{
+                  cursor: "pointer",
+                  marginBottom: "1rem",
+                  color: "#FFF",
+                  background:
+                    "linear-gradient(to right, #3861C9 5%, #56CAFB 95%)",
+                  padding: "1rem",
+                  borderRadius: "5px",
+                  fontSize: "2rem",
+                }}
+              >
+                Apagar meus dados
+              </button>
+            </form>
+          )}
+          <NavLink to="/" style={{ marginBottom: "2rem" }}>
+            Voltar
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
 }
